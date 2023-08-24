c436859c096c4b908ca53ba972cecfcb9ee82a1c
Juergen Adams <juergen@jekyll.one>
2023-08-24 20:04:14 +0200

    New version 2023.6.17

---
+++ _plugins/asciidoctor/conum.rb
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+# ~/_plugins/asciidoctor-extensions/conum.rb
+# Asciidoctor extension for conums
+#
+# Product/Info:
+# https://jekyll.one
+#
+# Copyright (C) 2023 Juergen Adams
+#
+# J1 Theme is licensed under the MIT License.
+# See: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
+#
+# ------------------------------------------------------------------------------
+require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+include Asciidoctor
+
+# A inline macro that embeds a conum into the output document
+#
+# Usage
+# ------------------------------------------------------------------------------
+#   conum::<num>[]
+#
+# Example:
+#
+#   conum:1[]
+#
+# ------------------------------------------------------------------------------
+Asciidoctor::Extensions.register do
+  inline_macro do
+    named :conum
+    process do |parent, target, attrs|
+      # TODO validate that this conum is valid
+      Asciidoctor::Inline.new(parent, :callout, target.to_i).convert
+    end
+  end
+end
---
+++ _plugins/asciidoctor/panel.rb
@@ -0,0 +1,43 @@
+# ------------------------------------------------------------------------------
+# ~/_plugins/asciidoctor-extensions/banner.rb
+# Asciidoctor extension for J1 Banner blocks
+#
+# Product/Info:
+# https://jekyll.one
+#
+# Copyright (C) 2023 Juergen Adams
+#
+# J1 Theme is licensed under the MIT License.
+# See: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
+#
+# ------------------------------------------------------------------------------
+require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+include Asciidoctor
+
+# A block macro that embeds a banner block into the output document
+#
+# Usage
+#
+#   banner::banner_id[role="additional classes"]
+#
+# Example:
+#
+#   banner::home_teaser_banner[role="mb-5"]
+#
+Asciidoctor::Extensions.register do
+
+  class J1BlockMacro < Extensions::BlockMacroProcessor
+    use_dsl
+
+    named :panel
+    name_positional_attributes 'role'
+
+    def process parent, target, attrs
+      html = %(<div id="#{target}" class="#{attrs['role']}"></div>)
+      create_pass_block parent, html, attrs, subs: nil
+    end
+  end
+
+  block_macro J1BlockMacro
+
+end
